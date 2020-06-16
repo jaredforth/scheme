7cbe2b85c8349e6ea9fd4bc28864cf774ffcb6ff
Jared Forth <jaredforthdev@gmail.com>
2020-06-16 11:13:59 -0500

    add two args

--- hello.hs
+++ hello.hs
@@ -4,4 +4,12 @@
  main :: IO ()
  main = do
      args <- getArgs
-     putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1)
+     let arg1 = args !! 0
+     let arg2 = args !! 1
+     let a = read arg1 :: Integer
+     let b = read arg2 :: Integer
+     let resultI = a + b
+     let result = show resultI
+     putStrLn ("Result: " ++ result)
+    --  let resStr = show result
+    --  putStrLn ("The result is: " ++ resStr)
