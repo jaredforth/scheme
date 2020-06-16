333f5555249ae95fd5c53914cdcde6b0de48fa97
Jared Forth <jaredforthdev@gmail.com>
2020-06-16 11:28:58 -0500

    update parser

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -5,9 +5,12 @@ symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
 
 main :: IO ()
 main = do 
