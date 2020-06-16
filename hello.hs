8ab01717bc55c98b615f01f73ab1404af2ee69a7
Jared Forth <jaredforthdev@gmail.com>
2020-06-16 11:16:38 -0500

    print out name

--- hello.hs
+++ hello.hs
@@ -3,13 +3,6 @@
  
  main :: IO ()
  main = do
-     args <- getArgs
-     let arg1 = args !! 0
-     let arg2 = args !! 1
-     let a = read arg1 :: Integer
-     let b = read arg2 :: Integer
-     let resultI = a + b
-     let result = show resultI
-     putStrLn ("Result: " ++ result)
-    --  let resStr = show result
-    --  putStrLn ("The result is: " ++ resStr)
+    putStrLn "Hello, what's your name?"  
+    name <- getLine  
+    putStrLn ("Hey " ++ name ++ ", you rock!")  
