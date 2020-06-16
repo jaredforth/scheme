612830123d2ec0523796b4fd24dedbcb25bc2532
Jared Forth <jaredforthdev@gmail.com>
2020-06-16 11:26:07 -0500

    init simple

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -3,3 +3,13 @@ import System.Environment
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+main :: IO ()
+main = do 
+         (expr:_) <- getArgs
+         putStrLn (readExpr expr)
